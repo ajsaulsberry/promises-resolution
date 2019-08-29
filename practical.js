7448dfc5df03ee07c04278b86fbcfba7835dab34
A. J. Saulsberry <ajsaulsberry@gmail.com>
2019-08-29 10:00:41 -0400

    Tried returning promise from function.

--- practical.js
+++ practical.js
@@ -7,7 +7,20 @@ const fillGlass = new Promise((resolve, reject) => {
             reject('failed!');
         }
     }, 500); // Timeout represents the length of time it takes to pour a glass, more or less.
- });
+});
+
+function fillGlassAsync(filltime) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let level = Math.random();
+            if (level >= 0.7) {
+                resolve(Math.round(level * 100));
+            } else {
+                reject('failed!');
+            }
+        }, filltime); // Timeout represents the length of time it takes to pour a glass, more or less.    
+    });
+}
 
 class Glass {
     constructor(glassId, level) {
@@ -17,24 +30,26 @@ class Glass {
 }
   
 function serveGlass(curGlass, result) {
+    console.log(curGlass);
     console.log(`That's a good pour! Glass ${curGlass.GlassId} is ${result}% full. Drink up.`)
 }
 
-function returnGlass(curGlass, errorMsg) {
-    console.log(`That's a bad pour. Glass ${curGlass.GlassId} ${errorMsg} Try again.`);
+function returnGlass(errorMsg) {
+    console.log(`That's a bad pour. Glass ${errorMsg} Try again.`);
 }
 
-function pour(ordered) {
+function pour(ordered, filltime) {
     let attempted = 1;
-    while (attempted <= ordered) {
-        var curGlass = new Glass(attempted);
-        console.log(curGlass);
-        fillGlass.then(result => serveGlass(curGlass, result), errorMsg => returnGlass(curGlass, errorMsg));
+    while (attempted < ordered) {
+        // var curGlass = new Glass(attempted);
+        // console.log(curGlass);
+        var fillGlass = fillGlassAsync(filltime);
+        fillGlass.then(result => serveGlass(new Glass(attempted), result), errorMsg => returnGlass(errorMsg));
         attempted++;
     }
 }
 
-pour(3);
+pour(3, 500);
 
 // Problem: although a unique object is being created each iteration, 
 // only the last object is being used in the callback functions.
